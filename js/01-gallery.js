edab60b38843f8984e04cc945a4c45038a800ffe
Kristian <kris.dziamka@gmail.com>
2022-07-16 22:23:27 +0300

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
-er
+const div = document.querySelector(`.gallery`);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(images) {
+  return images.map(
+    (img) => `<div class="gallery__item">
+<a class="gallery__link" href="${img.original}">
+  <img
+    class="gallery__image"
+    src="${img.preview}"
+    data-source="${img.original}"
+    alt="${img.description}"
+  />
+</a>
+</div>`
+  ).join('');
+}
+
+div.innerHTML = galleryMarkup;
+
+
+div.addEventListener(`click`, onClick)
+
+function onClick (event){
+    event.preventDefault()
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+      const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
+
